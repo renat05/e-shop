dfbccace377437dd4b1f950121840a155f746697
Renat <renat.safiyev@mail.ru>
2024-06-22 14:43:19 +0400

    react pet

--- Desktop/pet/src/App.js
+++ Desktop/pet/src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from './Main';
-import Nav from './components/Navigation';
-import Footer from './components/Footer';
-import Product from './components/Product';
-import Favourites from './components/Favorites';
+import React from "react"
+import './index.css'
 
-function App(props) {
-  let {allCars} = props
-  return (
-    <BrowserRouter>
-     <Nav allCars={allCars}/>
-      <Footer/>
-       <Routes>
-        <Route path='favorites' element={<Favourites/>} />
-        <Route path="/" element={<Main />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
-    </BrowserRouter>
+const App = () => React.createElement(
+    'div',
+    {className: 'ww'},
+    'Привет,w мир!'
   );
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
-export default App;
+export default App
